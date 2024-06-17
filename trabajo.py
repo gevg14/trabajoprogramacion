b97c62ab7d9a737d777455cf591fca1322ad198b
alvaro <alv.oyarzun@duocuc.cl>
2024-06-17 15:16:24 -0400

    #prueba 1

--- trabajo.py
+++ trabajo.py
@@ -1,1 +1,14 @@
-print("")
+import main as funcion
+#Integrantes: Gerardo Vera / Alvaro Oyarzun 
+listanombres=[]; 
+listaapellido=[]; 
+#declaracion de variables 
+#mostrador de datos 
+while True: 
+    nombre=(input("Ingrese nombre del trabajador--> ")) 
+    listanombres.append(nombre); 
+    apellido=(input("Ingrese apellido del trabajador---> ")); 
+    listaapellido.append(apellido); 
+    cargo=(input("Ingrese el cargo--> "));
+    break
+print()
